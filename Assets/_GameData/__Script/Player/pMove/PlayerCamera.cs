b5564dc783beb9dfc922f54c5d11d416d77e1636
Kyrylo <beatxprod@gmail.com>
2025-05-26 11:27:43 +0300

    update PlayerMove && implement PlayerCamera

--- Assets/_GameData/__Script/Player/pMove/PlayerCamera.cs
+++ Assets/_GameData/__Script/Player/pMove/PlayerCamera.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
 
-public class PlayerCamera : MonoBehaviour
+namespace Player.pMove
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerCamera : MonoBehaviour
     {
+        private const float zoomSpeed = 10f;
+        private const float minZoom = 60f;
+        private const float maxZoom = 120f;
+        private const float smoothTime = 0.2f;
+        private const float zoomSensitivity = 3f;
         
-    }
+        private Camera _camera;
+        private float targetFOV;
+        private float currentVelocity;
+        
+        private void Start()
+        {
+            _camera = GetComponent<Camera>();
+            _camera.fieldOfView = 90f;
+            targetFOV = _camera.fieldOfView;
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void LateUpdate()
+        {
+            float scroll = Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+
+            if (scroll != 0)
+            {
+                targetFOV -= scroll * zoomSpeed;
+                targetFOV = Mathf.Clamp(targetFOV, minZoom, maxZoom);
+            }
+            
+            _camera.fieldOfView = Mathf.SmoothDamp(_camera.fieldOfView, targetFOV, ref currentVelocity, smoothTime);
+        }
     }
 }
--- Assets/_GameData/__Script/Player/pMove/PlayerMove.cs
+++ Assets/_GameData/__Script/Player/pMove/PlayerMove.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Player.pMove
@@ -5,6 +6,7 @@ namespace Player.pMove
     public class PlayerMove : MonoBehaviour
     {
         [SerializeField] private float moveSpeed = 3f;
+        [SerializeField] private float sensitivity = 3;
         private Rigidbody _rb;
 
         private Vector3 _moveDir;
@@ -27,11 +29,22 @@ namespace Player.pMove
             _rb.linearVelocity = transform.TransformDirection(MoveDirection(_moveDir));;
         }
 
+        private void LateUpdate()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+            PlayerRotation(mouseX);
+        }
+
         private Vector3 MoveDirection(Vector3 inputMoveDir)
         {
             float finalSpeed = moveSpeed * Mathf.Clamp01(inputMoveDir.magnitude);
             
             return inputMoveDir.normalized * finalSpeed;
+        }
+        
+        private void PlayerRotation(float mouseX)
+        {
+            this.transform.Rotate(Vector3.up * mouseX);
         }
     }
 }
