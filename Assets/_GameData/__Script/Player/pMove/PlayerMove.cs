d03ae87fcd81df48900bbf7db6a1af7354e17d00
Kyrylo <beatxprod@gmail.com>
2025-05-26 11:05:08 +0300

    implement PlayerMove script

--- Assets/_GameData/__Script/Player/pMove/PlayerMove.cs
+++ Assets/_GameData/__Script/Player/pMove/PlayerMove.cs
@@ -4,6 +4,34 @@ namespace Player.pMove
 {
     public class PlayerMove : MonoBehaviour
     {
+        [SerializeField] private float moveSpeed = 3f;
+        private Rigidbody _rb;
+
+        private Vector3 _moveDir;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
         
+        private void Update()
+        {
+            float inputX = Input.GetAxis("Horizontal");
+            float inputZ = Input.GetAxis("Vertical");
+            
+            _moveDir = new Vector3(inputX, 0f, inputZ);
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.linearVelocity = transform.TransformDirection(MoveDirection(_moveDir));;
+        }
+
+        private Vector3 MoveDirection(Vector3 inputMoveDir)
+        {
+            float finalSpeed = moveSpeed * Mathf.Clamp01(inputMoveDir.magnitude);
+            
+            return inputMoveDir.normalized * finalSpeed;
+        }
     }
 }
